b69574e120783981e8955c17c01ec393fdb64c5a
dips98 <upadhayay.dipesh@gmail.com>
2023-06-11 17:41:37 +0530

    Method overloading

---
+++ Method OverLoading/OverLoading.java
@@ -0,0 +1,27 @@
+class MethodOverLoading{
+    public float add(float a, float b) {
+        return a+b;
+    }
+    public double add(double a, double b) {
+        return a+b;
+    }
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public int add(int a, int b, int c) {
+        return a+b+c;
+    }
+    public char add(char a, char b) {
+        return (char) (a+b);
+    }
+}
+
+public class OverLoading {
+    public static void main(String[] args) {
+        MethodOverLoading obj = new MethodOverLoading();
+        int result = obj.add(5,7);
+        float resultFloat = (float) obj.add(4.6, 7.9);
+        System.out.println(result);
+        System.out.println(resultFloat);
+    }
+}
