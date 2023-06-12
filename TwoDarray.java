284ff2b88d4e5ac17eea6b5341898719078252b9
dips98 <upadhayay.dipesh@gmail.com>
2023-06-12 13:14:01 +0530

    Two d array

---
+++ TwoDarray.java
@@ -0,0 +1,13 @@
+public class TwoDarray {
+    public static void main(String[] args) {
+        // intialzing two d array
+        // int a [][] = new int[2][2];
+        int a [][] = {
+            {2,5,6},
+            {8,9,6},
+            {2,4,6}
+        };
+        // a[0][0] = 5;
+        System.out.println(a[2][1]);
+    }
+}
