2ee90d1551b4bc24006e01360cd6ece09d9c69fd
dips98 <upadhayay.dipesh@gmail.com>
2023-06-13 19:43:50 +0530

    Comments

--- String/CompareString.java
+++ String/CompareString.java
@@ -1,20 +1,24 @@
 package String;
 
+// Where ever we use new keyword to create object of String class it will allocate always different refernece address. This will allow duplicate.
+
+// If you declare the String with out new keyword it will craete refference address in scp(String Constant Pool). That will never allow dupliacte
+
 public class CompareString {
     public static void main(String[] args) {
         String s1 = "Dipesh";
-        String s2 =new String("Dipesh");
+        String s2 = new String("Dipesh");
 
-        // USing == operator  it will check the reference address
-        if(s1==s2){
+        // USing == operator it will check the reference address
+        if (s1 == s2) {
             System.out.println("True both s1 and s2 is same");
-        }else{
+        } else {
             System.out.println("Both are not same have different reference address");
         }
 
         // Using equals will cheeck the inside content of the string and give the value
 
-        if(s1.equals(s2)){
+        if (s1.equals(s2)) {
             System.out.println("True s1 content is equal to  s2");
         }
     }
