599ffdc02775295a1643ed19bf5f992d22da178a
dips98 <upadhayay.dipesh@gmail.com>
2023-06-15 00:00:38 +0530

    number with string

--- String/ConcatinationString.java
+++ String/ConcatinationString.java
@@ -1,17 +1,19 @@
 package String;
 
-class ConcatinationString{
+class ConcatinationString {
     public static void main(String[] args) {
         String s1 = new String("Dipesh");
-        // if not assign to s1 after concatenation it will make new string in heap but not reffer to s1 
-        // Thats why we reffered to s1 to get change in s1 
-        s1 = s1.concat(" Upadhyay"); 
+        // if not assign to s1 after concatenation it will make new string in heap but
+        // not reffer to s1
+        // Thats why we reffered to s1 to get change in s1
+        s1 = s1.concat(" Upadhyay");
         System.out.println(s1);
 
         // It will create the object in String constant pool bcz not used new keyword
         String s2 = "Dipesh";
 
-        // s3 will be created in heap area bcz we use inbuild method concat which will acts as new key word
+        // s3 will be created in heap area bcz we use inbuild method concat which will
+        // acts as new key word
         // And will form in heap memmory pointing s3
         String s3 = s2.concat(" Upadhyay");
 
@@ -23,8 +25,7 @@ class ConcatinationString{
         System.out.println(s3);
         System.out.println(s4);
 
-
-        // +  operator
+        // + operator
 
         String w1 = "Dipesh";
         String w2 = w1 + " Upadhyay";
@@ -32,6 +33,8 @@ class ConcatinationString{
         System.out.println(w1);
         System.out.println(w2);
         System.out.println(w3);
-        System.out.println(w1+w2+w3);
+        System.out.println(w1 + w2 + w3);
+        // It will add number as string if passed + with string
+        System.out.println("Hello" + 5);
     }
 }
