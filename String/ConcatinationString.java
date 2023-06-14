5fe0e1c02937bc4ca5a885a0085a28afa7b29508
dips98 <upadhayay.dipesh@gmail.com>
2023-06-14 23:44:04 +0530

    More example

--- String/ConcatinationString.java
+++ String/ConcatinationString.java
@@ -7,5 +7,21 @@ class ConcatinationString{
         // Thats why we reffered to s1 to get change in s1 
         s1 = s1.concat(" Upadhyay"); 
         System.out.println(s1);
+
+        // It will create the object in String constant pool bcz not used new keyword
+        String s2 = "Dipesh";
+
+        // s3 will be created in heap area bcz we use inbuild method concat which will acts as new key word
+        // And will form in heap memmory pointing s3
+        String s3 = s2.concat(" Upadhyay");
+
+        String s4 = new String("Dipesh");
+
+        s4 = s4.concat(" Upadhyay");
+
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
     }
 }
