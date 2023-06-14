b3066abd18f1052214cfeb89f35b30c201f64ab2
dips98 <upadhayay.dipesh@gmail.com>
2023-06-15 02:26:01 +0530

    Array  using to reverse string

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -3,8 +3,20 @@ package String;
 public class ReverseString {
     public static void main(String[] args) {
         String s1 = new String("My Name is Anthony Gonsalvis");
+        System.out.println("Before Reverse : "+ s1);
         String reversedString = reverseString(s1);
-        System.out.println(reversedString);
+        System.out.println("After  Reverse : "+reversedString);
+        String []a = s1.split("");
+        String arrayString = reverseArrayString(a);
+        System.out.println("This is array string reversed : " + arrayString);
+    }
+
+    private static String reverseArrayString(String []a) {
+        String res = "";
+        for(int i=a.length-1;i>=0;i--){
+            res = res + a[i];
+        }
+        return res;
     }
 
     public static String reverseString(String  s1){
