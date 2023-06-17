c57db97776b6044532cb43e2f40148775dfe3251
dips98 <upadhayay.dipesh@gmail.com>
2023-06-17 23:36:20 +0530

    Hello world

--- Question/Sample.java
+++ Question/Sample.java
@@ -1,5 +1,9 @@
 package Question;
 
 public class Sample {
-    
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println("Hello");
+    }
 }
