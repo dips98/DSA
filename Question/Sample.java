7716f1b309035eb4387508fe18cb08b085b0f237
dips98 <upadhayay.dipesh@gmail.com>
2023-06-17 23:36:45 +0530

    H

--- Question/Sample.java
+++ Question/Sample.java
@@ -3,7 +3,6 @@ package Question;
 public class Sample {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println();
         System.out.println("Hello");
     }
 }
