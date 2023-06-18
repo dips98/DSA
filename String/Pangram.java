7ef6239c2da87ad5778b43b759f40b312529049a
dips98 <upadhayay.dipesh@gmail.com>
2023-06-18 23:26:36 +0530

    Pangram

--- String/Pangram.java
+++ String/Pangram.java
@@ -2,7 +2,27 @@ package String;
 
 public class Pangram {
     public static void main(String[] args) {
-        String str1 = "THE QUICK BROWN FOX JUMPS OVER LAZY DOG";
-        System.out.println(str1);
+        String str1 = "bawdsjogflickquartzvexnympih";
+        System.out.println(checkPangram(str1));
     }
+    public static boolean checkPangram(String str) {
+    str = str.toLowerCase();
+    boolean[] alphabet = new boolean[26];
+
+    for (char c : str.toCharArray()) {
+        if (Character.isLetter(c)) {
+            int index = c - 'a';
+            alphabet[index] = true;
+        }
+    }
+
+    for (boolean letter : alphabet) {
+        if (!letter) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 }
