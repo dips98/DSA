c17035e64b11b3d31ff545a4dfa63633a4b3cb38
dips98 <upadhayay.dipesh@gmail.com>
2023-06-18 19:47:52 +0530

    Workng code for palindrome

--- String/Palindrome.java
+++ String/Palindrome.java
@@ -2,6 +2,16 @@ package String;
 
 public class Palindrome {
     public static void main(String[] args) {
-        
+        String str1 = "22422";
+        String str2 = "22422";
+        String res = "";
+        for(int i=str1.length()-1;i>=0;i--){
+            res = res+str1.charAt(i);
+        }
+        if(res.equals(str2)){
+            System.out.println("Its a palindrome");
+        }else{
+            System.out.println("Its not a palindrome");
+        }
     }
 }
