9ef0eef558f58869e434c6464d741dadeef4d161
dips98 <upadhayay.dipesh@gmail.com>
2023-06-18 19:58:38 +0530

    Removed

--- String/Panagram.java
+++
@@ -1,8 +0,0 @@
-package String;
-
-public class Panagram {
-    public static void main(String[] args) {
-        String str1 = "THE QUICK BROWN FOX JUMPS OVER LAZY DOG";
-        System.out.println(str1);
-    }
-}
