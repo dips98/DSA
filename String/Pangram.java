c71913ca37ea8e6b828ffafed827b7fcadaccca7
dips98 <upadhayay.dipesh@gmail.com>
2023-06-18 19:58:25 +0530

    Its Pangram

---
+++ String/Pangram.java
@@ -0,0 +1,8 @@
+package String;
+
+public class Pangram {
+    public static void main(String[] args) {
+        String str1 = "THE QUICK BROWN FOX JUMPS OVER LAZY DOG";
+        System.out.println(str1);
+    }
+}
