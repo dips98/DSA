0cfcc6777ee812440f1030b93d4f73030bb0f827
dips98 <upadhayay.dipesh@gmail.com>
2023-06-18 19:49:01 +0530

    No need of second string

--- String/Palindrome.java
+++ String/Palindrome.java
@@ -3,12 +3,11 @@ package String;
 public class Palindrome {
     public static void main(String[] args) {
         String str1 = "22422";
-        String str2 = "22422";
         String res = "";
         for(int i=str1.length()-1;i>=0;i--){
             res = res+str1.charAt(i);
         }
-        if(res.equals(str2)){
+        if(res.equals(str1)){
             System.out.println("Its a palindrome");
         }else{
             System.out.println("Its not a palindrome");
