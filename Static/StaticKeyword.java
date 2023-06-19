38e63f4174a14d5c14b78d7844a3a9d20416f326
dips98 <upadhayay.dipesh@gmail.com>
2023-06-19 19:29:45 +0530

    Restored

---
+++ Static/StaticKeyword.java
@@ -0,0 +1,20 @@
+package Static;
+public class StaticKeyword {
+    static String name;
+    static int age = 25;
+    static{
+        name = "Dipesh";
+        age = 26;
+        System.out.println("Static block phele aegaa");
+        System.out.println(name + "'s age is "+ age);
+        show();
+    }
+    static void show(){
+        System.out.println("Showing");
+    }
+    public static void main(String[] args) {
+        int a = 45;
+        System.out.println("Main method abhi aya! "+ a);
+        show();
+    }
+}
