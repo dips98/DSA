67de6d1fdcf0778acfa1f226ce9381b61d473ff9
dips98 <upadhayay.dipesh@gmail.com>
2023-06-19 13:30:03 +0530

    Final

---
+++ String/StaticVar.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StaticVar {
+    public static void main(String[] args) {
+        final int a = 16;
+        // a = 89;
+        System.out.println(a);
+        // you cant re assign the variable name after using final
+        // it like const in javascrip.
+    }
+}
