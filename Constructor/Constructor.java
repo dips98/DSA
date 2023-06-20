3d7f2feda6c7b69f2a8091a9b0e8ff3b9a2edc38
dips98 <upadhayay.dipesh@gmail.com>
2023-06-20 23:51:10 +0530

    Format

--- Constructor/Constructor.java
+++ Constructor/Constructor.java
@@ -1,25 +1,31 @@
 package Constructor;
 
-class Demo{
+class Demo {
     int a = 45;
     int b = 24;
 
-    Demo(int a, int b){
-        System.out.println("First Contructor will execute"+ " Given Parameter :"+a);
+    Demo(int a, int b) {
+        System.out.println("First Contructor will execute" + " Given Parameter :" + a);
         this.a = this.a + a;
-        this.b = this.b+b;
+        this.b = this.b + b;
     }
-    void show(){
-        System.out.println(a +" "+ b);
+
+    void show() {
+        System.out.println(a + " " + b);
+    }
+
+    void change() {
+        a = a - 100;
+        b = b - 100;
     }
 }
 
 public class Constructor {
     public static void main(String[] args) {
-        Demo d = new Demo(56,1);
+        Demo d = new Demo(56, 1);
         d.show();
-        Demo d1  = new Demo(1,2);
+        Demo d1 = new Demo(1, 2);
+        d1.change();
         d1.show();
-
     }
 }
