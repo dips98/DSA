a44a5cd972e5e971cd9d78948a6558f467311776
dips98 <upadhayay.dipesh@gmail.com>
2023-06-20 23:33:51 +0530

    Constructor not created its bydefault

--- Constructor/Constructor.java
+++ Constructor/Constructor.java
@@ -1,5 +1,17 @@
 package Constructor;
 
+class Demo{
+    int a = 45;
+    int b = 24;
+
+    void show(){
+        System.out.println(a +" "+ b);
+    }
+}
+
 public class Constructor {
-    
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        d.show();
+    }
 }
