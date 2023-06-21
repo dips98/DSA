f590fe0e7430e1866adb59684267696da951bcc5
dips98 <upadhayay.dipesh@gmail.com>
2023-06-21 23:36:45 +0530

    Constructor

--- Constructor/ConstructorOverloading.java
+++ Constructor/ConstructorOverloading.java
@@ -1,6 +1,13 @@
 package Constructor;
 
+class Student{
+    int age;
+    String name;
 
+    Student(){
+        System.out.println("This is constructor");
+    }
+}
 
 public class ConstructorOverloading {
     public static void main(String[] args) {
