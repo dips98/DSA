fe949043b40ca7e986cf119a9e6f07d6c4bdc27c
dips98 <upadhayay.dipesh@gmail.com>
2023-06-23 01:43:02 +0530

    Getters and setters

--- Encapsulation/Encapsulation.java
+++ Encapsulation/Encapsulation.java
@@ -4,12 +4,30 @@ class Farmer{
     private int land;
     private String name;
 
-    void setName(String name){
-        // if we use same name it will treat as local varaible bydefault
-        // this issue called shadowing problem 
-        // it can be solved by this keyword
+    public int getLand() {
+        return land;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setLand(int land) {
+        this.land = land;
+    }
+    public void setName(String name) {
         this.name = name;
     }
+    // void setName(String name){
+    //     // if we use same name it will treat as local varaible bydefault
+    //     // this issue called shadowing problem 
+    //     // it can be solved by this keyword
+    //     this.name = name;
+    // }
+    // void setLand(int land){
+    //     // if we use same name it will treat as local varaible bydefault
+    //     // this issue called shadowing problem 
+    //     // it can be solved by this keyword
+    //     this.land = land;
+    // }
     void show(){
         System.out.println("Land :"+land+"\n"+"Name :"+name);
     }
@@ -19,6 +37,8 @@ public class Encapsulation {
     public static void main(String[] args) {
         Farmer f = new Farmer();
         f.setName("DOLLY");
-        f.show();
+        // f.show();
+        System.out.println(f.getName());
+
     }
 }
