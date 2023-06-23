d00d767b59df1d192b9a7fb3b17bf69bc9b88184
dips98 <upadhayay.dipesh@gmail.com>
2023-06-24 01:52:56 +0530

    Foramt - refactor

--- Oops/Inheritance/Inheritance.java
+++ Oops/Inheritance/Inheritance.java
@@ -1,24 +1,36 @@
 package Oops.Inheritance;
 
-class Human{ // Base class , Super class, parent class
+class Human { // Base class , Super class, parent class
     int age;
+    private String Name;
 
-    Human(){
+    Human() {
         System.out.println("Human 's Constructor");
     }
-    Human(int a){
+
+    Human(int a) {
         System.out.println("Overloading with ange param!");
         age = a;
     }
-    void sleep(){
-        System.out.println("Human need sleep to perform well in bed if his age is "+age);
+
+    void sleep() {
+        System.out.println("Human need sleep to perform well in bed if his age is " + age);
     }
 }
 
-class Student extends Human{ // sub class , child class // derived class
-    Student(){
+class Student extends Human { // sub class , child class // derived class
+    Student() {
         System.out.println("this is Student Contructor");
     }
+
+    void display() {
+        System.out.println("The age is " + age);
+        // System.out.println("The Name is "+ name); // private is not allowed in
+        // inhertance
+    }
+
+    // Contructor won't inherted in class Due to super() it calling parent class
+    // contructor then child contructer eill execute in student class example
 }
 
 public class Inheritance {
