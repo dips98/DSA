8585f4dd6c787f79bd19b589dbde6f33cf6a62db
dips98 <upadhayay.dipesh@gmail.com>
2023-06-25 00:38:29 +0530

    Polymorphism setting

---
+++ Oops/Polymorphism/Polymorphism.java
@@ -0,0 +1,39 @@
+package Oops.Polymorphism;
+
+class Aeroplane { // parent class methods become inherit method to child class
+    void takeOff() {
+        System.out.println("Taking off!");
+    }
+
+    void fly() {
+        System.out.println("Going to fly!");
+    }
+}
+
+class CargoPlane extends Aeroplane {
+    void takeOff() { // Defining same method name of inhert method in child class is called
+                     // over-riding method
+        System.out.println("Cargo plane is getting takeoff!");
+    }
+
+    void carryGoods() { // Defined new method in child class is called specialised method
+        System.out.println("Carrying goods!");
+    }
+}
+
+class PassengerPlane extends Aeroplane {
+    void takeOff() { // Defining same method name of inherit method in child class is called
+                     // over-riding method
+        System.out.println("Passenger plane is getting takeoff!");
+    }
+
+    void carryPassenger() { // Defined new method in child class is called specialised method
+        System.out.println("Carrying passengers!");
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+
+    }
+}
