f53cb1ca2f8dd79d99f78515be0e4e9928c0b2d1
dips98 <upadhayay.dipesh@gmail.com>
2023-06-24 15:23:03 +0530

    Refactor

--- Oops/Inheritance/hierarchicalInheritance.java
+++ Oops/Inheritance/hierarchicalInheritance.java
@@ -1,27 +1,33 @@
 package Oops.Inheritance;
-class A{
-    A(){
+
+class A {
+    A() {
         System.out.println("A constructor!");
     }
-    void sayHi(){
+
+    void sayHi() {
         System.out.println("Hi");
     }
 }
-class B extends A{
-    B(){
+
+class B extends A {
+    B() {
         System.out.println("B constructor");
     }
 }
-class C extends A{
-    C(){
+
+class C extends A {
+    C() {
         System.out.println("C constructor");
     }
-} 
-class D extends A{
-    D(){
+}
+
+class D extends A {
+    D() {
         System.out.println("D constructor");
     }
-} 
+}
+
 public class hierarchicalInheritance {
     public static void main(String[] args) {
         D d = new D();
