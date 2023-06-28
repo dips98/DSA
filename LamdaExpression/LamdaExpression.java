bb8df65aafd71d36a48f27bfd5ec6a0872e54b28
dips98 <upadhayay.dipesh@gmail.com>
2023-06-29 00:05:49 +0530

    Using inner class

--- LamdaExpression/LamdaExpression.java
+++ LamdaExpression/LamdaExpression.java
@@ -2,17 +2,23 @@ package LamdaExpression;
 // Inner class
 // Inner class have three types 1.Member(Without static)  2.Static 3.Anomymous
 // Static can only work in inner class
+interface I{
+    public void display();
+}
 class M{
     public void show(){
         System.out.println("Show");
     }
-    class N{
-
+    class N implements I{
+        public void display(){
+            System.out.println("Display !");
+        }
     }
 }
 public class LamdaExpression {
     public static void main(String[] args) {
         M m = new M();
         m.show();
+        
     }
 }
