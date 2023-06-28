776bd395805361bbde99e195f7862a99e3f9814e
dips98 <upadhayay.dipesh@gmail.com>
2023-06-28 23:43:05 +0530

    Comments

--- LamdaExpression/LamdaExpression.java
+++ LamdaExpression/LamdaExpression.java
@@ -1,6 +1,6 @@
 package LamdaExpression;
 // Inner class
-// Inner class have three types 1.Member 2.Static 3.Anomymous
+// Inner class have three types 1.Member(Without static)  2.Static 3.Anomymous
 // Static can only work in inner class
 class M{
 
