a78d9a1dc12798c0293c10e54486d78fb2ab79b7
dips98 <upadhayay.dipesh@gmail.com>
2023-06-28 23:58:05 +0530

    Method declare

--- LamdaExpression/LamdaExpression.java
+++ LamdaExpression/LamdaExpression.java
@@ -3,7 +3,9 @@ package LamdaExpression;
 // Inner class have three types 1.Member(Without static)  2.Static 3.Anomymous
 // Static can only work in inner class
 class M{
-
+    public void show(){
+        System.out.println("Show");
+    }
     class N{
 
     }
