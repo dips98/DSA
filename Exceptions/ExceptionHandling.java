20640fc0d0b3f3c85b8267429887e7870e198c52
dips98 <upadhayay.dipesh@gmail.com>
2023-07-02 00:39:53 +0530

    Comments

---
+++ Exceptions/ExceptionHandling.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+// Types of error
+// 1. Syntax error
+// 2. Logical error
+// 3. Run time error
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        
+    }
+}
