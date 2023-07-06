8c1ad6f4c7c3071121af5602bb1a3eac097b776a
dips98 <upadhayay.dipesh@gmail.com>
2023-07-06 23:43:33 +0530

    Solving problem

--- Collections/ArrayList/ArrList.java
+++ Collections/ArrayList/ArrList.java
@@ -1,6 +1,7 @@
 package Collections.ArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrList {
@@ -32,5 +33,53 @@ public class ArrList {
         arr1.add("All");
         arr1.add(80);
         System.out.println(arr1);
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(arr1);
+
+        System.out.println(result);
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Sorting the array in ascending order
+        Arrays.sort(nums);
+
+        // Logic to find triplets that sum up to zero
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // Skipping duplicates
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skipping duplicates
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    // Moving pointers
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
     }
 }
