9dc1e39e8b5b8e34dc710a540f939fb836bbb8a2
dips98 <upadhayay.dipesh@gmail.com>
2023-07-06 13:26:47 +0530

    Some basic functions

--- Collections/ArrayList/ArrList.java
+++ Collections/ArrayList/ArrList.java
@@ -14,11 +14,16 @@ public class ArrList {
         arr.add(7);
         arr.add(8);
         // arr.add("HI"); You cant add we can make arraylist int staring char as we wanted else it can be accept any data type include array itself 
-
+        arr.ensureCapacity(20);
         for(int i:arr){
             System.out.println(i);
         }
         System.out.println(arr);
+        System.out.println(arr.contains(3));
+        System.out.println("index of number 3 is :"+arr.indexOf(3));
+        System.out.println("Size of array :"+ arr.size());
+        arr.trimToSize();
+        System.out.println(arr.size());
 
     }
 }
