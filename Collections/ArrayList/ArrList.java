e464744b809b2e92de79492b2e16d31db9b47f9d
dips98 <upadhayay.dipesh@gmail.com>
2023-07-06 12:49:39 +0530

    Expermiment

--- Collections/ArrayList/ArrList.java
+++ Collections/ArrayList/ArrList.java
@@ -1,7 +1,23 @@
 package Collections.ArrayList;
 
+import java.util.ArrayList;
+
 public class ArrList {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> arr = new ArrayList();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
+        arr.add(7);
+        arr.add(8);
+
+        for(int i:arr){
+            System.out.println(i);
+        }
+        System.out.println(arr);
+
     }
 }
