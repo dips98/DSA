373c7fe50dd5c3e5779adaa51d5d2225921d4862
dips98 <upadhayay.dipesh@gmail.com>
2023-07-06 23:44:13 +0530

    Removed

--- Collections/ArrayList/ArrList.java
+++ Collections/ArrayList/ArrList.java
@@ -38,48 +38,4 @@ public class ArrList {
 
         System.out.println(result);
     }
-
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        // Sorting the array in ascending order
-        Arrays.sort(nums);
-
-        // Logic to find triplets that sum up to zero
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                // Skipping duplicates
-                continue;
-            }
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // Skipping duplicates
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-
-                    // Moving pointers
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
-        }
-
-        return result;
-    }
 }
