26dc8b74236eb56beaa2e652cc9a78163dd40407
dips98 <upadhayay.dipesh@gmail.com>
2023-07-08 16:54:02 +0530

    LinkedList

---
+++ Collections/LinkedList/LinkedListL.java
@@ -0,0 +1,13 @@
+package Collections.LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListL {
+    public static void main(String[] args) {
+        LinkedList Ll = new LinkedList();
+        Ll.add(1);
+        Ll.add("KO");
+        Ll.add('s');
+        System.out.println(Ll);
+    }
+}
