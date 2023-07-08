a6610aede951c546a155deec08ff3c8a378d7fe0
dips98 <upadhayay.dipesh@gmail.com>
2023-07-09 01:00:48 +0530

    Deque

--- Collections/Deque/Deque.java
+++ Collections/Deque/Deque.java
@@ -5,7 +5,14 @@ import java.util.ArrayDeque;
 public class Deque {
     public static void main(String[] args) {
         ArrayDeque  aq = new ArrayDeque();
-        aq.add("aq")
+        aq.add("aq");
+        aq.addFirst("90");
+        aq.addLast(89);
+
+
+        aq.offer(9);
+        aq.offerFirst(0);
+        aq.offerLast(64);
         System.out.println(aq);
     }
 }
