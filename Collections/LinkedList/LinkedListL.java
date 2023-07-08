c477d91601f7bb69d8ff4ea79547c47f6fd92f95
dips98 <upadhayay.dipesh@gmail.com>
2023-07-08 17:01:54 +0530

    Comments

--- Collections/LinkedList/LinkedListL.java
+++ Collections/LinkedList/LinkedListL.java
@@ -2,6 +2,7 @@ package Collections.LinkedList;
 
 import java.util.LinkedList;
 
+// Linked llist  implements List and Deque interface so its having more extra methods than array list
 public class LinkedListL {
     public static void main(String[] args) {
         LinkedList Ll = new LinkedList();
