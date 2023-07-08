e0a5822fc0563ee8f899585afc08bb28aaa8d86b
dips98 <upadhayay.dipesh@gmail.com>
2023-07-09 00:53:58 +0530

    Added Deque folder

---
+++ Collections/Deque/Deque.java
@@ -0,0 +1,11 @@
+package Collections.Deque;
+
+import java.util.ArrayDeque;
+
+public class Deque {
+    public static void main(String[] args) {
+        ArrayDeque  aq = new ArrayDeque();
+        aq.add("aq")
+        System.out.println(aq);
+    }
+}
