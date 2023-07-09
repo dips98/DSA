b152fd7a9c756d73180b86386a75f315c9d4197f
dips98 <upadhayay.dipesh@gmail.com>
2023-07-09 18:10:00 +0530

    Notes

--- Collections/PriorityQue/PriorrityQ.java
+++ Collections/PriorityQue/PriorrityQ.java
@@ -6,7 +6,10 @@ public class PriorrityQ {
     public static void main(String[] args) {
         PriorityQueue pq = new PriorityQueue();
         // pq.add("pq");  string not taking 
-        // Its use binary search tree
+        // Its use Minimnum heap data structure
+
+        // it  will store first like  binary tree
+        
         pq.add(1);
         pq.add(78);
         pq.add(99);
