453ce1efeef4f3ddb8dc871a100ce62c9478a2c2
dips98 <upadhayay.dipesh@gmail.com>
2023-07-09 18:10:16 +0530

    Tree

---
+++ Collections/Tree/Tree.java
@@ -0,0 +1,20 @@
+package Collections.Tree;
+
+import java.util.TreeSet;
+
+public class Tree {
+    public static void main(String[] args) {
+        // It used binray search tree balanced tree
+        TreeSet ts = new TreeSet();
+        ts.add(33);
+        ts.add(135);
+        ts.add(55);
+        ts.add(65);
+        ts.add(80);
+        ts.add(105);
+        ts.add(150);
+        ts.add(130);
+
+        System.out.println(ts);
+    }
+}
