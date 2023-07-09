90d07005bf2448affd44a9387bbd61ae117c6e8e
dips98 <upadhayay.dipesh@gmail.com>
2023-07-10 00:17:57 +0530

    Iterable using for loop first

---
+++ Collections/Iterators/Iterators.java
@@ -0,0 +1,27 @@
+package Collections.Iterators;
+
+import java.util.ArrayList;
+
+public class Iterators {
+    public static void main(String[] args) {
+        ArrayList ar = new ArrayList();
+        ar.add(14);
+        ar.add(34);
+        ar.add(54);
+        ar.add(43);
+        ar.add(23);
+        ar.add(90);
+        ar.add(75);
+        ar.add(99);
+        System.out.println(ar);
+
+        // printing through for loop
+        
+        for (int i =0;i<ar.size();i++){
+            // Object o = ar.get(i);
+            // System.out.println(o);
+            System.out.println(ar.get(i));
+        }
+
+    }
+}
