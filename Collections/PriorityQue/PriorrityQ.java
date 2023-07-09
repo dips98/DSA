3f91d6844bd07d1af03abd8dc868fd5780b820b9
dips98 <upadhayay.dipesh@gmail.com>
2023-07-09 18:00:57 +0530

    Notes

--- Collections/PriorityQue/PriorrityQ.java
+++ Collections/PriorityQue/PriorrityQ.java
@@ -6,6 +6,7 @@ public class PriorrityQ {
     public static void main(String[] args) {
         PriorityQueue pq = new PriorityQueue();
         // pq.add("pq");  string not taking 
+        // Its use binary search tree
         pq.add(1);
         pq.add(78);
         pq.add(99);
