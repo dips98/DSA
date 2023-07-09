4145a4f864489412e28c92c653b450998af53b08
dips98 <upadhayay.dipesh@gmail.com>
2023-07-09 12:54:17 +0530

    Priority  queue

---
+++ Collections/PriorityQue/PriorrityQ.java
@@ -0,0 +1,17 @@
+package Collections.PriorityQue;
+
+import java.util.PriorityQueue;
+
+public class PriorrityQ {
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        // pq.add("pq");  string not taking 
+        pq.add(1);
+        pq.add(78);
+        pq.add(99);
+        pq.add(123);
+        pq.add(123);
+        pq.add(90);
+        System.out.println(pq);
+    }
+}
