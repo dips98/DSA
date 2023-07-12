82a1f771f269b449ea5a3f014f63288b59781b37
dips98 <upadhayay.dipesh@gmail.com>
2023-07-12 23:55:33 +0530

    Comments

--- MultiThreading/MultiThread.java
+++ MultiThreading/MultiThread.java
@@ -4,6 +4,6 @@ package MultiThreading;
 // 1
 public class MultiThread {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Multi");
     }
 }
