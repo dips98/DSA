b4f774d90418c5b9208f7dd3bbe6bf7a604af5e8
dips98 <upadhayay.dipesh@gmail.com>
2023-07-13 23:37:55 +0530

    Notes

--- MultiThreading/MultiThread.java
+++ MultiThreading/MultiThread.java
@@ -3,7 +3,7 @@ package MultiThreading;
 // Prerequisites
 // 1.Opps 2.OS
 
-// Multi Tasking with diffrent application(Process) in OS called "Process-Based"/'Context Switching' Multi Tasking
+// Multi Tasking with diffrent application(Process) in OS called "Process-Based/Context Switching" Multi Tasking
 // Within Application(Process) doing multi tasking in Os is called "Thread-Based/Multi-Threading" Multi Tasking
 public class MultiThread {
     public static void main(String[] args) {
