ae66b1b5e20a2e19c3544ec118cb30392127f5c9
dips98 <upadhayay.dipesh@gmail.com>
2023-07-13 23:26:24 +0530

    Notes

--- MultiThreading/MultiThread.java
+++ MultiThreading/MultiThread.java
@@ -1,7 +1,7 @@
 package MultiThreading;
 
 // Prerequisites
-// 1.Opps
+// 1.Opps 2.OS
 public class MultiThread {
     public static void main(String[] args) {
         System.out.println("Multi Threading !");
