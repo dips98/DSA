d07de0cfecad97b36ac4ba21ce4e0dba9e6c60f4
dips98 <upadhayay.dipesh@gmail.com>
2023-07-14 23:26:19 +0530

    Commments

--- MultiThreading/MultiThread.java
+++ MultiThreading/MultiThread.java
@@ -11,7 +11,7 @@ package MultiThreading;
 public class MultiThread {
     public static void main(String[] args) {
         System.out.println("Multi Threading !");
-        Thread t = new Thread(); // Creating Thread Ob
+        Thread t = new Thread(); // Creating Thread Obj
         System.out.println(t.getThreadGroup());
     }
 }
