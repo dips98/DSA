0d41664b623dc4f97253422faeb5496fe5c71f63
dips98 <upadhayay.dipesh@gmail.com>
2023-07-21 23:52:18 +0530

    comments

--- MultiThreading/MultiThread.java
+++ MultiThreading/MultiThread.java
@@ -10,7 +10,7 @@ package MultiThreading;
 
 public class MultiThread {
     public static void main(String[] args) {
-        // System.out.println("Multi
+        // System.out.println("Mult
         System.out.println("Main Thread");
         String name = Thread.currentThread().getName();
         System.out.println(name);
