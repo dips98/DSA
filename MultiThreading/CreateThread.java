a4c1ad398e0103dca00b7b5aa17edca5fb1ffe8b
dips98 <upadhayay.dipesh@gmail.com>
2023-08-11 22:36:05 +0530

    Comments

---
+++ MultiThreading/CreateThread.java
@@ -0,0 +1,8 @@
+package MultiThreading;
+
+//  Two ways to create the Threads in java
+// 1. By extending the Thread class 2. By implementing runable Interface
+
+public class CreateThread {
+    
+}
