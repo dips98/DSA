fa8f67f0ea785b4ffc9a867af35fe16908055111
dips98 <upadhayay.dipesh@gmail.com>
2023-08-11 22:39:25 +0530

    Creating class by exdends with Thread

--- MultiThreading/CreateThread.java
+++ MultiThreading/CreateThread.java
@@ -3,6 +3,12 @@ package MultiThreading;
 //  Two ways to create the Threads in java
 // 1. By extending the Thread class 2. By implementing runable Interface
 
+
+class Mythread extends Thread{
+    
+}
+
+
 public class CreateThread {
     public static void main(String[] args) {
         
