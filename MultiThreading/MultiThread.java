b4aa4773682c77720bf5e286310762ad7284f797
dips98 <upadhayay.dipesh@gmail.com>
2023-08-11 22:23:28 +0530

    Get priority of threads

--- MultiThreading/MultiThread.java
+++ MultiThreading/MultiThread.java
@@ -13,6 +13,6 @@ public class MultiThread {
         System.out.println("Main Thread");
         String name = Thread.currentThread().getName();
         System.out.println("The name of main thread is " + name);
-
+        System.out.println("Priority of this thread is "+ Thread.currentThread().getPriority());
     }
 }
