7f587ea28f53826cf9f0d2aea30be45edda14988
dips98 <upadhayay.dipesh@gmail.com>
2023-08-12 00:58:45 +0530

    Hashmap

---
+++ MapAndGenerics/Map.java
@@ -0,0 +1,13 @@
+package MapAndGenerics;
+
+import java.util.*;
+
+public class Map {
+    public static void main(String[] args) {
+        HashMap h = new HashMap();
+        Hashtable ht = new Hashtable();
+        LinkedHashMap lhm = new LinkedHashMap();
+        TreeMap tm = new TreeMap();
+        // System.out.println(h);
+    }
+}
