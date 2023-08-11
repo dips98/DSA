8a9b4409e6cfc209fa72c5f4157e6f06d0e93f6c
dips98 <upadhayay.dipesh@gmail.com>
2023-08-11 22:26:46 +0530

    Priority changed

--- MultiThreading/MultiThread.java
+++ MultiThreading/MultiThread.java
@@ -10,9 +10,17 @@ package MultiThreading;
 public class MultiThread {
     public static void main(String[] args) {
         // System.out.printl
-        System.out.println("Main Thread");
+        System.out.println("Before changing: ");
         String name = Thread.currentThread().getName();
         System.out.println("The name of main thread is " + name);
         System.out.println("Priority of this thread is "+ Thread.currentThread().getPriority());
+
+        System.out.println("after changing :");
+        Thread.currentThread().setName("Dipesh");
+        Thread.currentThread().setPriority(2);
+        String name1 = Thread.currentThread().getName();
+        System.out.println("The name of main thread is " + name1);
+        System.out.println("Priority of this thread is "+ Thread.currentThread().getPriority());
+
     }
 }
