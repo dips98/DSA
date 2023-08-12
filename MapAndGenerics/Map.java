4b59bf12c328ce17b873baac202ae80ed6d11f4e
dips98 <upadhayay.dipesh@gmail.com>
2023-08-12 23:46:13 +0530

    Space

--- MapAndGenerics/Map.java
+++ MapAndGenerics/Map.java
@@ -10,7 +10,7 @@ public class Map {
         TreeMap tm = new TreeMap();
 
         h.put(01, "Dips ");
-        h.put(02, "Rips");
+        h.put(02, "Rips ");
         h.put(03, "Sips");
         h.put(04, "Lips");
         System.out.println(h);
