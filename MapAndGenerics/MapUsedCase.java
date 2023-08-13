766587b1470e7b10d081fb06c7577ffb6d2bfff7
dips98 <upadhayay.dipesh@gmail.com>
2023-08-13 12:35:21 +0530

    Hashmap

--- MapAndGenerics/MapUsedCase.java
+++ MapAndGenerics/MapUsedCase.java
@@ -1,8 +1,11 @@
 package MapAndGenerics;
 
+import java.util.*;
 
 public class MapUsedCase {
     public static void main(String[] args) {
+        HashMap hm = new HashMap();
+        hm.put(1, "Sam");
         
     }
 }
