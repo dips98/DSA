4bf1b3bab220bd840c3f91afaec88c58dc09138c
dips98 <upadhayay.dipesh@gmail.com>
2023-08-13 13:17:24 +0530

    Iterating with set iterator

--- MapAndGenerics/MapUsedCase.java
+++ MapAndGenerics/MapUsedCase.java
@@ -6,7 +6,18 @@ public class MapUsedCase {
     public static void main(String[] args) {
         HashMap hm = new HashMap();
         hm.put(1, "Sam");
+        hm.put(2, "Dam");
+        hm.put(3, "Cam");
+        hm.put(4, "Lam");
+        hm.put(5, "Pam");
+        hm.put(6, "Nam");
+        hm.put(7, "Iom");
         System.out.println(hm.get(1));
         System.out.println("-----------------------------");
+        Set keys = hm.keySet();
+        Iterator it1 = keys.iterator();
+        while(it1.hasNext()){
+            System.out.println(it1.next());
+        }
     }
 }
