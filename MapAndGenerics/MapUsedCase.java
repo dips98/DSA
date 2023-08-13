a89306774b3dc74877054de3e6d1cc84f91005d7
dips98 <upadhayay.dipesh@gmail.com>
2023-08-13 13:08:22 +0530

    Get by key

--- MapAndGenerics/MapUsedCase.java
+++ MapAndGenerics/MapUsedCase.java
@@ -6,7 +6,8 @@ public class MapUsedCase {
     public static void main(String[] args) {
         HashMap hm = new HashMap();
         hm.put(1, "Sam");
-        // System.out.println(hm);
-
+        System.out.println(hm.get(1));
+        System.out.println("-----------------------------");
+        // Iterator itr = hm.
     }
 }
