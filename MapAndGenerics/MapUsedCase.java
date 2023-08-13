cfe9b35332464ed099f8e0a65ec73a698aa69981
dips98 <upadhayay.dipesh@gmail.com>
2023-08-13 12:38:34 +0530

    Map

--- MapAndGenerics/MapUsedCase.java
+++ MapAndGenerics/MapUsedCase.java
@@ -6,6 +6,6 @@ public class MapUsedCase {
     public static void main(String[] args) {
         HashMap hm = new HashMap();
         hm.put(1, "Sam");
-        
+        System.out.println(hm);
     }
 }
