2b400fa0e315ee795b47d8132cf152d015fac6b2
dips98 <upadhayay.dipesh@gmail.com>
2023-08-16 23:31:32 +0530

    Init

--- MapAndGenerics/MapMore.java
+++ MapAndGenerics/MapMore.java
@@ -39,6 +39,6 @@ class Student {
 public class MapMore {
     public static void main(String[] args) {
         Student s1 = new Student("Dipesh", 24, "Mumbai");
-        Student
+        Student s2 = 
     }
 }
